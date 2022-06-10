e55dfd0c6aa015b3324d82f22302714bb03cad75
VladVegera <94718115+VladVegera@users.noreply.github.com>
2022-06-10 17:24:29 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,27 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+  private:
+    T array[100];
+    int top;
+  public:
+    TStack():top(-1) { }
+    T get() const {
+      return array[top];
+    }
+    bool isEmpty() const {
+      return top == -1;
+    }
+    bool isFull() const {
+      return top == size - 1;
+    }
+    void pop() {
+      if (top >= 0)
+        top--;
+    }
+    void push(T item) {
+      if (top < size - 1)
+        array[++top] = item;
+    }
 };
-
 #endif  // INCLUDE_TSTACK_H_
