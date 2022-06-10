474e4922b9e59b5208186685b5d5756cd208c780
VladVegera <94718115+VladVegera@users.noreply.github.com>
2022-06-10 17:57:28 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,10 +4,10 @@
 
 template<typename T, int size>
 class TStack {
-   private:
+   private: 
     T array[100];
     int top;
-   public:
+   public: 
     TStack():top(-1) { }
     T get() const {
       return array[top];
